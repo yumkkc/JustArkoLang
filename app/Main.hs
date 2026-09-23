e64f7c579c028683c774556312a682ebe0ad32c0
Saiyam Chettri <saiyum303@gmail.com>
2026-09-23 06:59:33 +0530

    initial commit

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import MyLib
+
+main :: IO ()
+main = do
+  putStrLn "Hello, Bebba!"
+  someFunc
---
+++ src/Lisp.hs
@@ -0,0 +1,9 @@
+module Lisp where
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+        deriving Show
