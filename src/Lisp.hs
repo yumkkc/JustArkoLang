04897bd52a9032716c267ac68cbfa859569f9849
yumkkc <saiyum303@gmail.com>
2026-09-23 11:38:47 +0530

    Implement Numbers and String
    1. Add Parser to have feature for quotes in string
    2. Add multiple number feature, hexa, octa, decimal and binary

--- src/Lisp.hs
+++ src/Lisp.hs
@@ -1,9 +1,15 @@
 module Lisp where
 
+data LispNum = Decimal Integer
+             | Hexa  String
+             | Octal Integer
+             | Binary Integer
+          deriving Show
+
 data LispVal = Atom String
              | List [LispVal]
              | DottedList [LispVal] LispVal
-             | Number Integer
+             | Number LispNum
              | String String
              | Bool Bool
         deriving Show
--- src/MyLib.hs
+++ src/MyLib.hs
@@ -17,9 +17,12 @@ readExpr input = case parse parseExpr "bebba" input of
   Right val -> "Found value : " ++ show val
 
 
+parseQuote :: Parser Char
+parseQuote = char '\\' >> char '"'
+
 parseString :: Parser LispVal
 parseString = do char '"'
-                 x <- many (noneOf "\"")
+                 x <- many $ (try parseQuote <|> noneOf "\"")
                  char '"'
                  return $ String x
 
@@ -33,12 +36,31 @@ parseAtom = do first <- letter <|> symbol
                  otherwise -> Atom atom
 
 
-parseNumber :: Parser LispVal
-parseNumber = Number . read <$> many1 digit
+parseRadixNotHex :: Parser Char
+parseRadixNotHex = option 'd' (char '#' >> oneOf "dbo")
 
+parseNumberWithRadixNotHex :: Parser LispVal
+parseNumberWithRadixNotHex = do radix <- parseRadixNotHex
+                                rest  <- many1 digit
+                                return $ case radix of 
+                                      'd' -> (Number . Decimal . read) rest
+                                      'b' -> (Number . Binary . read) rest
+                                      'o' -> (Number . Octal . read) rest
+                                
+hexVals :: Parser Char
+hexVals = oneOf "abcdefABCDEF" <|> digit
+
+parseHex :: Parser LispVal
+parseHex = do _ <- char '#'
+              _ <- char 'x'
+              vals <- many1 hexVals
+              return $ (Number . Hexa) vals
+
+parseNumber :: Parser LispVal              
+parseNumber = try parseHex <|> parseNumberWithRadixNotHex
 
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseString <|> parseNumber <|> parseAtom 
 
 
 someFunc :: IO ()
